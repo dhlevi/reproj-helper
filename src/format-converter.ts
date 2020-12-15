1c05a3bc95366c9e849a216c8b462a98d0a7d371
dhemsworth <dylan.hemsworth@gmail.com>
2020-12-14 17:27:40 -0800

    Simple wkt converter

---
+++ src/format-converter.ts
@@ -0,0 +1,295 @@
+import { Feature, FeatureCollection, Geometry, Position } from "geojson"
+import { deepCopy } from "./deep-copy"
+
+/**
+ * FormatConverter is a utilit class that assists with converting
+ * some spatial formats to others.
+ * 
+ * Currently supports converting from/to Well Known Text and GeoJson
+ * I'm sure you're immediately wondering why I did WKT Parsing without Regex
+ * Me too... me too...
+ */
+export default class FormatConverter {
+  private sourceWkt: string | null = null
+  private sourceJson: FeatureCollection | Feature | Geometry | null = null
+
+  /**
+   * Static instance constructor
+   */
+  public static instance (): FormatConverter {
+    return new FormatConverter()
+  }
+
+  /**
+   * Supply a WKT string to convert
+   * This automatically converts to GeoJson
+   * @param wkt Your WKT string
+   */
+  public fromWkt (wkt: string): FormatConverter {
+    this.sourceWkt = wkt.trim()
+
+    this.sourceJson = {
+      type: 'Feature',
+      properties: {},
+      geometry: this.convertWktToJson()
+    }
+
+    return this
+  }
+
+  /**
+   * Converts to Well Known Text
+   */
+  public toWkt (): string {
+    return this.convertToWkt()
+  }
+
+  /**
+   * Convert a geojson feature
+   * @param json GeoJSON feature
+   */
+  public fromGeoJson (json: FeatureCollection | Feature | Geometry): FormatConverter {
+    this.sourceJson = deepCopy(json)
+    return this
+  }
+
+  /**
+   * Get the converted data as GeoJSON
+   */
+  public toGeoJson (): FeatureCollection | Feature | Geometry | null {
+    return this.sourceJson
+  }
+
+  /**************************/
+  /* WKT to JSON conversion */
+  /**************************/
+
+  private convertWktToJson (): Geometry {
+    if (!this.sourceWkt || this.sourceWkt.length === 0) {
+      throw new Error('No WKT data supplied')
+    }
+
+    const type = this.sourceWkt.split(' ')[0].trim().toUpperCase()
+    const typeMod = this.sourceWkt.split(' ')[1].trim().toUpperCase()
+    const unsupported = ['EMPTY', 'ZM', 'M'].includes(typeMod)
+
+    if (unsupported) {
+      throw Error('Geometry is empty or using an unupported type!')
+    }
+
+    const data = this.sourceWkt.substring(this.sourceWkt.indexOf('(') + 1, this.sourceWkt.length - 1) // get the string, minus open/close brackets.
+    
+    return this.buildWktGeometry(type, data)
+  }
+
+  private buildWktGeometry (type: string, data: string): Geometry {
+    try {
+      switch (type) {
+        case 'POINT': {
+          return {
+            type: 'Point',
+            coordinates: this.parseWktCoord(data)
+          }
+        }
+        case 'MULTIPOINT': {
+          // remove every last bracket from the string
+          const coords = data.trim().replace(/\(/g, '').replace(/\)/g, '')
+          return {
+            type: 'MultiPoint',
+            coordinates: this.parseWktLine(coords)
+          }
+        }
+        case 'LINESTRING': {
+          return {
+            type: 'LineString',
+            coordinates: this.parseWktLine(data)
+          }
+        }
+        case 'MULTILINESTRING': {
+          return {
+            type: 'MultiLineString',
+            coordinates: this.parseWktRing(data) // Multi Linestring is identical to Polygon
+          }
+        }
+        case 'POLYGON': {
+          return {
+            type: 'Polygon',
+            coordinates: this.parseWktRing(data)
+          }
+        }
+        case 'MULTIPOLYGON': {
+          const multiPolyGeom: Geometry = {
+            type: 'MultiPolygon',
+            coordinates: []
+          }
+  
+          const polygons = data.split(')),')
+          for (const poly of polygons) {
+            // ensure double brackets are gone, and the string ends with a bracket (might have been trimmed by the split)
+            const cleanPoly = poly.replace('((', '(') + (poly.endsWith(')') ? '' : ')')
+            multiPolyGeom.coordinates.push(this.parseWktRing(cleanPoly.trim()))
+          }
+          
+          return multiPolyGeom
+        }
+        case 'GEOMETRYCOLLECTION': {
+          const geomCollection: Geometry = {
+            type: 'GeometryCollection',
+            geometries: []
+          }
+
+          // for a geometry collection, we need to split up the geoms in the list.
+          // we can't split by ',' or even '),' though, because it's possible that
+          // we'd have multifeature geoms or poly's with rings.
+
+          const idxData = data.toUpperCase().replace(/POINT/g, '-POINT').replace(/LINESTRING/g, '-LINESTRING').replace(/POLYGON/g, '-POLYGON').replace(/MULTI-/g, '-MULTI').trim()
+          const geoms = idxData.split('-')
+          for (const geom of geoms) {
+            if (geom && geom.length > 0) {
+              let cleanGeom = geom.trim().toUpperCase()
+              // remove any trailing commas
+              if (cleanGeom.endsWith(',')) {
+                cleanGeom = cleanGeom.substring(0, cleanGeom.length - 1)
+              }
+
+              const geomType = cleanGeom.split(' ')[0].trim()
+              const typeMod = cleanGeom.split(' ')[1].trim()
+              const unsupported = ['EMPTY', 'ZM', 'M'].includes(typeMod)
+
+              if (unsupported) {
+                throw Error('Geometry is empty or using an unupported type!')
+              }
+              
+              const geomData = cleanGeom.substring(cleanGeom.indexOf('(') + 1, cleanGeom.length - 1) // get the string, minus open/close brackets.
+
+              geomCollection.geometries.push(this.buildWktGeometry(geomType, geomData))
+            }
+          }
+
+          return geomCollection
+        }
+        default: { 
+          throw new Error(`WKT type of ${type} is not currently supported`)
+        } 
+      }
+    } catch (err) {
+      throw Error(err)
+    }
+  }
+
+  // Parsing for WKT to json
+  private parseWktCoord (coord: string): number[] {
+    return [parseFloat(coord.trim().split(' ')[0]), parseFloat(coord.trim().split(' ')[1])]
+  }
+
+  // Parsing a Line WKT to json
+  private parseWktLine (line: string): number[][] {
+    const lineCoords = []
+    const coords = line.trim().split(',')
+    for(const coord of coords) {
+      lineCoords.push(this.parseWktCoord(coord))
+    }
+
+    return lineCoords
+  }
+
+  // Parsing a ring/polygon interior or exterior
+  private parseWktRing (poly: string): number[][][] {
+    const ringCoords = []
+    const rings = poly.split('),')
+    let idx = 0
+    for(const ring of rings) {
+      // ring length, but -1 if we end in a bracket
+      const cleanedRing = ring.trim().substring(ring.trim().indexOf('(') + 1, ring.trim().length - (ring.endsWith(')') ? 1 : 0));
+      ringCoords[idx] = this.parseWktLine(cleanedRing)
+      idx += 1
+    }
+    return ringCoords
+  }
+
+  /*****************
+   * Json to WKT
+   *****************/
+
+  private convertToWkt (): string {
+    if (this.sourceJson) {
+      if (this.sourceJson.type === 'FeatureCollection') {
+        let wktString = 'GEOMETRYCOLLECTION ('
+        for (const childFeature of this.sourceJson.features) {
+          wktString += `${this.wktStringFromGeometry(childFeature.geometry)}, `
+        }
+        return wktString.substring(0, wktString.length - 2) + ')'
+      } else if (this.sourceJson.type === 'Feature') {
+        return this.wktStringFromGeometry(this.sourceJson.geometry)
+      } else {
+        return this.wktStringFromGeometry(this.sourceJson)
+      }
+    }
+
+    return ''
+  }
+
+  private wktStringFromGeometry (geometry: Geometry): string {
+    switch (geometry.type) {
+      case 'Point': {
+        return `POINT (${this.toWktCoordString(geometry.coordinates)})`
+      }
+      case 'MultiPoint': {
+        return `MULTIPOINT (${this.lineToWktString(geometry.coordinates)})`
+      }
+      case 'LineString': {
+        return `LINESTRING (${this.lineToWktString(geometry.coordinates)})`
+      }
+      case 'MultiLineString': {
+        return `MULTILINESTRING (${this.ringToWktString(geometry.coordinates)})`
+      }
+      case 'Polygon': {
+        return `POLYGON (${this.ringToWktString(geometry.coordinates)})`
+      }
+      case 'MultiPolygon': {
+        return `MULTIPOLYGON (${this.polygonToWktString(geometry.coordinates)})`
+      }
+      case 'GeometryCollection': {
+        let wktString = 'GEOMETRYCOLLECTION ('
+        for (const childGeometry of geometry.geometries) {
+          wktString += `${this.wktStringFromGeometry(childGeometry)}, `
+        }
+        return wktString.substring(0, wktString.length - 2) + ')'
+      }
+    }
+  }
+
+  private polygonToWktString(coordinates: Position[][][]): string {
+    let coordString = ''
+
+    for (const coord of coordinates) {
+      coordString += `(${this.ringToWktString(coord)}), `
+    }
+
+    return coordString.substring(0, coordString.length - 2)
+  }
+
+  private ringToWktString(coordinates: Position[][]): string {
+    let coordString = ''
+
+    for (const coord of coordinates) {
+      coordString += `(${this.lineToWktString(coord)}), `
+    }
+
+    return coordString.substring(0, coordString.length - 2)
+  }
+
+  private lineToWktString(coordinates: Position[]): string {
+    let coordString = ''
+
+    for (const coord of coordinates) {
+      coordString += `${this.toWktCoordString(coord)}, `
+    }
+
+    return coordString.substring(0, coordString.length - 2)
+  }
+
+  private toWktCoordString(coordinate: Position): string {
+    return `${coordinate[0]} ${coordinate[1]}`
+  }
+}
---
+++ test/converter.test.ts
@@ -0,0 +1,184 @@
+import { Feature, LineString, MultiLineString, MultiPoint, MultiPolygon, Point, Polygon } from "geojson"
+import FormatConverter from "../src/format-converter"
+
+describe('format-converter.ts', () => {
+  it('Test fail on empty', async () => {
+    const converter = new FormatConverter()
+
+    let failed = false
+    try {
+      const result = converter.fromWkt('POINT EMPTY').toGeoJson() as Feature
+      console.log(JSON.stringify(result))
+    } catch (err) {
+      failed = true
+    }
+
+    expect(failed).toBe(true)
+  })
+  it('Test wkt point conversion', async () => {
+    const converter = new FormatConverter()
+
+    const sourceWkt = 'POINT (0 0)'
+    const json = converter.fromWkt(sourceWkt).toGeoJson() as Feature
+
+    expect(json?.type).toBe('Feature')
+    expect(json?.geometry.type).toBe('Point')
+    expect((json?.geometry as Point).coordinates[0]).toBe(0)
+    expect((json?.geometry as Point).coordinates[1]).toBe(0)
+
+    const wktString = converter.fromGeoJson(json).toWkt()
+    expect(wktString).toBe(sourceWkt)
+  })
+  it('Test wkt MultiPoint conversion', async () => {
+    const converter = new FormatConverter()
+
+    const sourceWkt = 'MULTIPOINT (0 0, 1 1, 2 2)'
+    const json = converter.fromWkt(sourceWkt).toGeoJson() as Feature
+
+    expect(json?.type).toBe('Feature')
+    expect(json?.geometry.type).toBe('MultiPoint')
+    expect((json?.geometry as MultiPoint).coordinates[0][0]).toBe(0)
+    expect((json?.geometry as MultiPoint).coordinates[0][1]).toBe(0)
+    expect((json?.geometry as MultiPoint).coordinates[1][0]).toBe(1)
+    expect((json?.geometry as MultiPoint).coordinates[1][1]).toBe(1)
+    expect((json?.geometry as MultiPoint).coordinates[2][0]).toBe(2)
+    expect((json?.geometry as MultiPoint).coordinates[2][1]).toBe(2)
+
+    const wktString = converter.fromGeoJson(json).toWkt()
+    expect(wktString).toBe(sourceWkt)
+  })
+  it('Test wkt line conversion', async () => {
+    const converter = new FormatConverter()
+
+    const sourceWkt = 'LINESTRING (0 0, 1 1, 2 2)'
+    const json = converter.fromWkt(sourceWkt).toGeoJson() as Feature
+
+    expect(json?.type).toBe('Feature')
+    expect(json?.geometry.type).toBe('LineString')
+    expect((json?.geometry as LineString).coordinates[0][0]).toBe(0)
+    expect((json?.geometry as LineString).coordinates[0][1]).toBe(0)
+    expect((json?.geometry as LineString).coordinates[1][0]).toBe(1)
+    expect((json?.geometry as LineString).coordinates[1][1]).toBe(1)
+    expect((json?.geometry as LineString).coordinates[2][0]).toBe(2)
+    expect((json?.geometry as LineString).coordinates[2][1]).toBe(2)
+
+    const wktString = converter.fromGeoJson(json).toWkt()
+    expect(wktString).toBe(sourceWkt)
+  })
+  it('Test wkt multiline conversion', async () => {
+    const converter = new FormatConverter()
+
+    const sourceWkt = 'MULTILINESTRING ((0 0, 1 1, 2 2), (3 3, 4 4, 5 5))'
+    const json = converter.fromWkt(sourceWkt).toGeoJson() as Feature
+
+    expect(json?.type).toBe('Feature')
+    expect(json?.geometry.type).toBe('MultiLineString')
+    expect((json?.geometry as MultiLineString).coordinates[0][0][0]).toBe(0)
+    expect((json?.geometry as MultiLineString).coordinates[0][0][1]).toBe(0)
+    expect((json?.geometry as MultiLineString).coordinates[0][1][0]).toBe(1)
+    expect((json?.geometry as MultiLineString).coordinates[0][1][1]).toBe(1)
+    expect((json?.geometry as MultiLineString).coordinates[0][2][0]).toBe(2)
+    expect((json?.geometry as MultiLineString).coordinates[0][2][1]).toBe(2)
+
+    expect((json?.geometry as MultiLineString).coordinates[1][0][0]).toBe(3)
+    expect((json?.geometry as MultiLineString).coordinates[1][0][1]).toBe(3)
+    expect((json?.geometry as MultiLineString).coordinates[1][1][0]).toBe(4)
+    expect((json?.geometry as MultiLineString).coordinates[1][1][1]).toBe(4)
+    expect((json?.geometry as MultiLineString).coordinates[1][2][0]).toBe(5)
+    expect((json?.geometry as MultiLineString).coordinates[1][2][1]).toBe(5)
+
+    const wktString = converter.fromGeoJson(json).toWkt()
+    expect(wktString).toBe(sourceWkt)
+  })
+  it('Test wkt polygon conversion', async () => {
+    const converter = new FormatConverter()
+
+    const json = converter.fromWkt('POLYGON ((0 0, 1 1, 2 2, 0 0))').toGeoJson() as Feature
+
+    expect(json?.type).toBe('Feature')
+    expect(json?.geometry.type).toBe('Polygon')
+    expect((json?.geometry as Polygon).coordinates[0][0][0]).toBe(0)
+    expect((json?.geometry as Polygon).coordinates[0][0][1]).toBe(0)
+    expect((json?.geometry as Polygon).coordinates[0][1][0]).toBe(1)
+    expect((json?.geometry as Polygon).coordinates[0][1][1]).toBe(1)
+    expect((json?.geometry as Polygon).coordinates[0][2][0]).toBe(2)
+    expect((json?.geometry as Polygon).coordinates[0][2][1]).toBe(2)
+
+    const sourceWkt = 'POLYGON ((35 10, 45 45, 15 40, 10 20, 35 10), (20 30, 35 35, 30 20, 20 30))'
+    const interiorRingJson = converter.fromWkt(sourceWkt).toGeoJson() as Feature
+
+    expect(interiorRingJson?.type).toBe('Feature')
+    const geom = interiorRingJson?.geometry as Polygon
+    const exteriorRing = geom.coordinates[0]
+    const interiorRing = geom.coordinates[1]
+    expect(geom.type).toBe('Polygon')
+    expect(exteriorRing[0][0]).toBe(35)
+    expect(exteriorRing[0][1]).toBe(10)
+    expect(exteriorRing[1][0]).toBe(45)
+    expect(exteriorRing[1][1]).toBe(45)
+    expect(exteriorRing[2][0]).toBe(15)
+    expect(exteriorRing[2][1]).toBe(40)
+    expect(exteriorRing[3][0]).toBe(10)
+    expect(exteriorRing[3][1]).toBe(20)
+    expect(exteriorRing[4][0]).toBe(35)
+    expect(exteriorRing[4][1]).toBe(10)
+    // interior ring
+    expect(interiorRing[0][0]).toBe(20)
+    expect(interiorRing[0][1]).toBe(30)
+    expect(interiorRing[1][0]).toBe(35)
+    expect(interiorRing[1][1]).toBe(35)
+    expect(interiorRing[2][0]).toBe(30)
+    expect(interiorRing[2][1]).toBe(20)
+    expect(interiorRing[3][0]).toBe(20)
+    expect(interiorRing[3][1]).toBe(30)
+
+    const wktString = converter.fromGeoJson(interiorRingJson).toWkt()
+    expect(wktString).toBe(sourceWkt)
+  })
+  it('Test wkt multipolygon conversion', async () => {
+    const converter = new FormatConverter()
+
+    const sourceWkt = 'MULTIPOLYGON (((40 40, 20 45, 45 30, 40 40)), ((20 35, 10 30, 10 10, 30 5, 45 20, 20 35), (30 20, 20 15, 20 25, 30 20)))'
+    const interiorRingJson = converter.fromWkt(sourceWkt).toGeoJson() as Feature
+
+    expect(interiorRingJson?.type).toBe('Feature')
+    const geom = interiorRingJson?.geometry as MultiPolygon
+    const firstPoly = geom.coordinates[0]
+    expect(geom.type).toBe('MultiPolygon')
+
+    expect(firstPoly[0][0][0]).toBe(40)
+    expect(firstPoly[0][0][1]).toBe(40)
+    expect(firstPoly[0][1][0]).toBe(20)
+    expect(firstPoly[0][1][1]).toBe(45)
+    expect(firstPoly[0][2][0]).toBe(45)
+    expect(firstPoly[0][2][1]).toBe(30)
+    expect(firstPoly[0][3][0]).toBe(40)
+    expect(firstPoly[0][3][1]).toBe(40)
+
+    const secondPoly = geom.coordinates[1]
+    const exteriorRing = secondPoly[0]
+    const interiorRing = secondPoly[1]
+
+    expect(exteriorRing[0][0]).toBe(20)
+    expect(exteriorRing[0][1]).toBe(35)
+
+    expect(interiorRing[0][0]).toBe(30)
+    expect(interiorRing[0][1]).toBe(20)
+
+
+    const wktString = converter.fromGeoJson(interiorRingJson).toWkt()
+    expect(wktString).toBe(sourceWkt)
+  })
+  it('Test wkt Geometry collection conversion', async () => {
+    const converter = new FormatConverter()
+
+    const sourceWkt = 'GEOMETRYCOLLECTION (POINT (40 10), LINESTRING (10 10, 20 20, 10 40), POLYGON ((40 40, 20 45, 45 30, 40 40)))'
+    const json = converter.fromWkt(sourceWkt).toGeoJson() as Feature
+
+    expect(json?.type).toBe('Feature')
+    expect(json?.geometry.type).toBe('GeometryCollection')
+
+    const wktString = converter.fromGeoJson(json).toWkt()
+    expect(wktString).toBe(sourceWkt)
+  })
+})
